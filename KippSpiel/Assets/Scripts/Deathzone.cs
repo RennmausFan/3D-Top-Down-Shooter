57ef48dd6df7e81964714f53c963333c16b20343
RennmausFan <freakybrause@gmail.com>
2018-01-12 22:30:10 +0100

    (1.) Version 12.01.18
    Basic game

---
+++ KippSpiel/Assets/Scripts/Deathzone.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Deathzone : MonoBehaviour {
+
+    GameObject player;
+
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            player.SendMessage("Die");
+        }
+    }
+}
---
+++ KippSpiel/Assets/Scripts/ShootBullet.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootBullet : MonoBehaviour {
+
+    [SerializeField]
+    GameObject thisObj;
+
+    [SerializeField]
+    private GameObject hitParticle;
+
+    public float speed;
+    public float lifeTime = 12f;
+    public int damage = 25;
+
+    private float timePassed;
+
+	// Update is called once per frame
+	void Update () {
+        MoveBullet();
+        if (timePassed > lifeTime)
+        {
+            Destroy(thisObj);
+        }
+        timePassed += Time.deltaTime;
+
+	}
+
+    private void MoveBullet()
+    {
+        thisObj.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag != "Player" && other.gameObject.tag != "Bullet" && other.gameObject.tag != "IgnoreBullets")
+        {
+            Instantiate(hitParticle, thisObj.transform.position, Quaternion.identity);
+            Destroy(thisObj);
+        }
+    }
+
+}
