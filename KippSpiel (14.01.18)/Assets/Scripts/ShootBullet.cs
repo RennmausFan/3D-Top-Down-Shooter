7225df0bce1b602827e4432247f4e53b2e597a4b
RennmausFan <freakybrause@gmail.com>
2018-01-14 10:55:33 +0100

    (1.) Version 12.01.18

---
+++ KippSpiel (14.01.18)/Assets/Scripts/ShootBullet.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootBullet : MonoBehaviour {
+
+    [SerializeField]
+    GameObject thisObj;
+
+    [SerializeField]
+    private GameObject hitParticle;
+
+    public float speed;
+    public float lifeTime = 12f;
+    public int damage = 25;
+
+    private float timePassed;
+
+	// Update is called once per frame
+	void Update () {
+        MoveBullet();
+        if (timePassed > lifeTime)
+        {
+            Destroy(thisObj);
+        }
+        timePassed += Time.deltaTime;
+
+	}
+
+    private void MoveBullet()
+    {
+        thisObj.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Enemy bullets
+        if (gameObject.tag == "EnemyBullet" && other.gameObject.tag != "EnemyBullet" && other.gameObject.tag != "Enemy" && other.gameObject.tag != "IgnoreBullets")
+        {
+            Instantiate(hitParticle, thisObj.transform.position, Quaternion.identity);
+            Destroy(thisObj);
+        }
+        //Player bullets
+        else if (gameObject.tag == "Bullet" && other.gameObject.tag != "Player" && other.gameObject.tag != "Bullet" && other.gameObject.tag != "IgnoreBullets")
+        {
+            Instantiate(hitParticle, thisObj.transform.position, Quaternion.identity);
+            Destroy(thisObj);
+        }
+    }
+
+}
