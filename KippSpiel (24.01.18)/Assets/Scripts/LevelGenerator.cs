e497f4809ced7656ac15a26af4963e863e6128d1
RennmausFan <freakybrause@gmail.com>
2018-01-24 16:58:21 +0100

    Version 24.01.18

---
+++ KippSpiel (24.01.18)/Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject[] modules;
+
+    [SerializeField]
+    private GameObject[] cornerModules;
+
+    [SerializeField]
+    private GameObject startModul;
+    private GameObject start;
+
+    public int maxRoomDepth;
+    private int roomDepth;
+
+    // Use this for initialization
+    void Awake()
+    {
+        start = Instantiate(startModul, Vector3.zero, Quaternion.Euler(0, RandomRotation(), 0));
+        BuildLevel(start, direction.None);
+    }
+
+    //Recursiv method that creates level by attaching a modul at each door
+    void BuildLevel(GameObject modul, direction ignoreDirection)
+    {
+        print(roomDepth);
+        if (roomDepth >= maxRoomDepth)
+        {
+            return;
+        }
+        LinkedList<direction> doors = modul.GetComponent<ModulManager>().GetDoors();
+        if (doors.Contains(direction.Left) && ignoreDirection != direction.Left)
+        {
+            Vector3 pos = modul.transform.position + new Vector3(-30, 0, 0);
+            GameObject newModul = Instantiate(RandomModul(modules), pos, Quaternion.identity);
+            ModulManager script = newModul.GetComponent<ModulManager>();
+            script.RotateToDoor(direction.Right);
+            BuildLevel(newModul, direction.Right);
+            //roomDepth++;
+        }
+        if (doors.Contains(direction.Right) && ignoreDirection != direction.Right)
+        {
+            Vector3 pos = modul.transform.position + new Vector3(30, 0, 0);
+            GameObject newModul = Instantiate(RandomModul(modules), pos, Quaternion.identity);
+            ModulManager script = newModul.GetComponent<ModulManager>();
+            script.RotateToDoor(direction.Left);
+            BuildLevel(newModul, direction.Left);
+            //roomDepth++;
+        }
+        if (doors.Contains(direction.Up) && ignoreDirection != direction.Up)
+        {
+            Vector3 pos = modul.transform.position + new Vector3(0, 0, 30);
+            GameObject newModul = Instantiate(RandomModul(modules), pos, Quaternion.identity);
+            ModulManager script = newModul.GetComponent<ModulManager>();
+            script.RotateToDoor(direction.Down);
+            BuildLevel(newModul, direction.Down);
+            //roomDepth++;
+        }
+        if (doors.Contains(direction.Down) && ignoreDirection != direction.Down)
+        {
+            Vector3 pos = modul.transform.position + new Vector3(0, 0, -30);
+            GameObject newModul = Instantiate(RandomModul(modules), pos, Quaternion.identity);
+            ModulManager script = newModul.GetComponent<ModulManager>();
+            script.RotateToDoor(direction.Up);
+            BuildLevel(newModul, direction.Up);
+            //roomDepth++;
+        }
+        roomDepth++;
+    }
+
+    //Returns random modul from selected array
+    GameObject RandomModul(GameObject[] modules)
+    {
+        int random = (int)Random.Range(0f, modules.Length);
+        return modules[random];
+    }
+
+    //Returns a random rotation, either 0, 90, 180, 270
+    float RandomRotation()
+    {
+        float random = Random.Range(0f, 1f);
+        if (random <= 0.25f)
+        {
+            return 0f;
+        }
+        else if (random > 0.25f && random <= 0.5f)
+        {
+            return 90f;
+        }
+        else if (random > 0.5f && random <= 0.75f)
+        {
+            return 180f;
+        }
+        else
+        {
+            return -90f;
+        }
+    }
+}
---
+++ KippSpiel (24.01.18)/Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CubeActions _CubeActions;
+
+    public int cubeSpeed = 50;
+    public int dashPower = 1;
+    public int dashCount = 5;
+    public int dashMax = 5;
+    public float dashReloadSec;
+    public float fireRateStart;
+    public float fireRateMax = 0.05f;
+
+    [SerializeField]
+    private float fireRateSec;
+    
+    private float timePassed_Dash;
+    private float timePassed_Fire;
+
+    [SerializeField]
+    private Light cubeLight;
+
+    void Start()
+    {
+        _CubeActions = gameObject.GetComponent<CubeActions>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Tip Forward
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            _CubeActions.MoveCube(new Vector3(0, 0, cubeSpeed));
+
+        }
+        //Tip Backwards
+        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            _CubeActions.MoveCube(new Vector3(0, 0, -cubeSpeed));
+
+        }
+        //Tip Left
+        else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            _CubeActions.MoveCube(new Vector3(-cubeSpeed, 0, 0));
+
+        }
+        //Tip Right
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            _CubeActions.MoveCube(new Vector3(cubeSpeed, 0, 0));
+
+        }
+        //Dash
+        if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetMouseButtonDown(1))
+        {
+            if (dashCount > 0)
+            {
+                Vector3 direction = _CubeActions.GetMouseDirection().normalized;
+                if (direction == Vector3.zero)
+                {
+                    return;
+                }
+                _CubeActions.Dash(direction * dashPower);
+                dashCount--;
+                timePassed_Dash = 0;
+            }
+        }
+        //Rapid-Fire
+        else if (Input.GetMouseButton(0) || Input.GetKey(KeyCode.Space))
+        {
+            if (timePassed_Fire > fireRateSec)
+            {
+                _CubeActions.Shoot();
+                timePassed_Fire = 0;
+            }
+            timePassed_Fire += Time.deltaTime;
+        }
+        //Reload Dash
+        timePassed_Dash += Time.deltaTime;
+        if (timePassed_Dash > dashReloadSec && dashCount < dashMax)
+        {
+            dashCount++;
+            timePassed_Dash = 0;
+        }
+        //Adjust Light
+        cubeLight.intensity = Mathf.Lerp(cubeLight.intensity, dashCount, Time.deltaTime *5);
+        //Adjust fire rate by glow;
+        fireRateSec = fireRateStart - GetComponent<CubeActions>().GetGlow() / 1000f;
+        if (fireRateSec <= fireRateMax)
+        {
+            fireRateSec = 0.05f;
+        }
+    }
+}
